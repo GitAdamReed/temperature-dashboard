1b36c4dd121a1957d8575cefa087f15143ff9cc3
GitAdamReed <adamreed170@gmail.com>
2025-08-16 19:15:51 +0100

    Add GetCpuSensorData and GetGpuSensorData unit tests

--- MyHWMonitorWPFAppUnitTests/HardwareServiceUnitTests.cs
+++ MyHWMonitorWPFAppUnitTests/HardwareServiceUnitTests.cs
@@ -8,7 +8,7 @@ namespace MyHWMonitorWPFAppUnitTests
 {
     public class HardwareServiceUnitTests
     {
-        
+
         [SetUp]
         public void Setup()
         {
@@ -82,6 +82,117 @@ namespace MyHWMonitorWPFAppUnitTests
             var hwService = new HardwareService(stubComputer.Object);
 
             Assert.That(hwService.GpuName, Is.EqualTo("GPU"));
+        }
+
+        [Test]
+        public void GetCpuSensorData_WhenCpuSensorsHaveReadings_ReturnsCorrectData()
+        {
+            var stubSensor = new Mock<ISensor>();
+            stubSensor.SetupGet(s => s.SensorType).Returns(SensorType.Temperature);
+            stubSensor.SetupGet(s => s.Name).Returns("CPU Package");
+            float stubSensorValue = 50f;
+            float stubSensorMin = 25f;
+            float stubSensorMax = 75f;
+            stubSensor.SetupGet(s => s.Value).Returns(stubSensorValue);
+            stubSensor.SetupGet(s => s.Min).Returns(stubSensorMin);
+            stubSensor.SetupGet(s => s.Max).Returns(stubSensorMax);
+
+            var stubHardware = new Mock<IHardware>();
+            stubHardware.SetupGet(h => h.HardwareType).Returns(HardwareType.Cpu);
+            stubHardware.SetupGet(s => s.Sensors).Returns([stubSensor.Object]);
+
+            var stubComputer = new Mock<IComputer>();
+            stubComputer.SetupGet(c => c.Hardware).Returns([stubHardware.Object]);
+
+            var hwService = new HardwareService(stubComputer.Object);
+
+            var expectedSensorItem = new SensorItem
+            {
+                Name = "CPU Package",
+                Value = $"{stubSensorValue:F1}",
+                Min = $"{stubSensorMin:F1}",
+                Max = $"{stubSensorMax:F1}"
+            };
+            var (sensorItems, currentTemp) = hwService.GetCpuSensorData();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(currentTemp, Is.EqualTo(50f));
+                Assert.That(sensorItems[0].Name, Is.EqualTo(expectedSensorItem.Name));
+                Assert.That(sensorItems[0].Value, Is.EqualTo(expectedSensorItem.Value));
+                Assert.That(sensorItems[0].Min, Is.EqualTo(expectedSensorItem.Min));
+                Assert.That(sensorItems[0].Max, Is.EqualTo(expectedSensorItem.Max));
+            });
+        }
+
+        [Test]
+        public void GetCpuSensorData_WhenCpuIsNotDetected_ThrowsException()
+        {
+            var stubHardware = new Mock<IHardware>();
+            stubHardware.SetupGet(h => h.HardwareType).Returns(HardwareType.GpuNvidia);
+
+            var stubComputer = new Mock<IComputer>();
+            stubComputer.SetupGet(c => c.Hardware).Returns([stubHardware.Object]);
+
+            var hwService = new HardwareService(stubComputer.Object);
+
+            Assert.That(hwService.GetCpuSensorData, Throws.Exception.Message.Contains("Cannot detect CPU."));
+        }
+
+        [Test]
+        public void GetGpuSensorData_WhenGpuSensorsHaveReadings_ReturnsCorrectData()
+        {
+            var stubSensor = new Mock<ISensor>();
+            stubSensor.SetupGet(s => s.SensorType).Returns(SensorType.Temperature);
+            stubSensor.SetupGet(s => s.Name).Returns("GPU Core");
+            float stubSensorValue = 40f;
+            float stubSensorMin = 20f;
+            float stubSensorMax = 55f;
+            stubSensor.SetupGet(s => s.Value).Returns(stubSensorValue);
+            stubSensor.SetupGet(s => s.Min).Returns(stubSensorMin);
+            stubSensor.SetupGet(s => s.Max).Returns(stubSensorMax);
+
+            var stubHardware = new Mock<IHardware>();
+            stubHardware.SetupGet(h => h.HardwareType).Returns(HardwareType.GpuNvidia);
+            stubHardware.SetupGet(s => s.Sensors).Returns([stubSensor.Object]);
+
+            var stubComputer = new Mock<IComputer>();
+            stubComputer.SetupGet(c => c.Hardware).Returns([stubHardware.Object]);
+
+            var hwService = new HardwareService(stubComputer.Object);
+
+            var expectedSensorItem = new SensorItem
+            {
+                Name = "GPU Core",
+                Value = $"{stubSensorValue:F1}",
+                Min = $"{stubSensorMin:F1}",
+                Max = $"{stubSensorMax:F1}"
+            };
+
+            var (sensorItems, currentTemp) = hwService.GetGpuSensorData();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(currentTemp, Is.EqualTo(40f));
+                Assert.That(sensorItems[0].Name, Is.EqualTo(expectedSensorItem.Name));
+                Assert.That(sensorItems[0].Value, Is.EqualTo(expectedSensorItem.Value));
+                Assert.That(sensorItems[0].Min, Is.EqualTo(expectedSensorItem.Min));
+                Assert.That(sensorItems[0].Max, Is.EqualTo(expectedSensorItem.Max));
+            });
+        }
+
+        [Test]
+        public void GetGpuSensorData_WhenGpuIsNotDetected_ThrowsException()
+        {
+            var stubHardware = new Mock<IHardware>();
+            stubHardware.SetupGet(h => h.HardwareType).Returns(HardwareType.Cpu);
+
+            var stubComputer = new Mock<IComputer>();
+            stubComputer.SetupGet(c => c.Hardware).Returns([stubHardware.Object]);
+
+            var hwService = new HardwareService(stubComputer.Object);
+
+            Assert.That(hwService.GetGpuSensorData, Throws.Exception.Message.Contains("Cannot detect GPU."));
         }
     }
 }
